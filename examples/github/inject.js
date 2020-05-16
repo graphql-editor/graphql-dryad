672680b97436eae54af100ce9df04b7e9c529132
aexol <aexol@aexol.com>
2020-05-16 22:01:45 +0200

    dramatically updated example

--- examples/github/inject.js
+++ examples/github/inject.js
@@ -1,27 +1,74 @@
-/* CTRL/CMD + space in dryad
-to write overrides in
-string html templates
-inject function format is:
-
-(v:{
-    name: string
-    value: type of value
-    className: class assigned
-})
-    => string containing html
-
-(v) => 
-    `<div>${v.value}</div>` */
-
-dryad = {
-  Actor: {
-    avatarUrl: (v) => `<img
-            class="${v.className}"
-             src="${v.value}" />`,
-  },
-  User: {
-    avatarUrl: (v) => `<img
-            class="${v.className}"
-             src="${v.value}" />`,
-  },
-};
+const response = await Gql.query({
+  site: [
+    {},
+    {
+      host: true,
+      port: true,
+    },
+  ],
+  allMarkdownRemark: [
+    {},
+    {
+      nodes: {
+        excerpt: true,
+        frontmatter: {
+          custom: true,
+          title: true,
+          date: [
+            {
+              formatString: 'DD/MM/YYYY',
+            },
+            true,
+          ],
+          image: {
+            publicURL: true,
+          },
+          author: {
+            id: true,
+            avatar: {
+              publicURL: true,
+            },
+          },
+        },
+      },
+    },
+  ],
+});
+const posts = response.allMarkdownRemark.nodes.filter(
+  (n) => !n.frontmatter.custom,
+);
+const addHost = (publicURL) =>
+  `http://${response.site.host}:${response.site.port}${publicURL}`;
+return `
+  <div class="Feed">
+      ${posts
+        .map(
+          (p) => `
+      <vstack class="PostCard">
+          <div 
+              class="Image"
+              style="background-image:url('${addHost(
+                p.frontmatter.image.publicURL,
+              )}'" 
+          ></div>
+          <text class="title">${p.frontmatter.title}</text>
+          <text class="desc">${p.excerpt || ''}</text>
+          <hstack class="Footer" spacing=xs>
+              <text>${p.frontmatter.date}</text>
+              <spacer></spacer>
+              <text class="Written">Written by,<br> ${
+                p.frontmatter.author.id
+              }</text>
+              <div 
+              style="background-image: url(${addHost(
+                p.frontmatter.author.avatar.publicURL,
+              )});" 
+              class="Avatar"
+              ></div>
+          </hstack>
+      </vstack>
+      `,
+        )
+        .join('')}
+  </div>
+`;
--- sandbox/DefaultCss.tsx
+++
@@ -1,9 +0,0 @@
-export const DefaultCSS = `
-.d-object{
-    padding: 10px;
-}
-
-.Int{
-    color: #f99;
-}
-`;
