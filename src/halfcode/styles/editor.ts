d50034de4d78fc926a7c44727b361e7396087a79
aexol <aexol@aexol.com>
2020-09-28 10:01:43 +0200

    updated some css properties of monaco editor; zeus update;

--- src/halfcode/styles/editor.ts
+++ src/halfcode/styles/editor.ts
@@ -3,12 +3,6 @@ const css = (t: TemplateStringsArray): string => {
   return t.toString();
 };
 export const EditorRestyle = css`
-  .editor-widget {
-    position: fixed !important;
-  }
-  .context-view {
-    position: fixed !important;
-  }
   .monaco-aria-container {
     bottom: 0;
   }
